a36f808ecfe1de89679e83b2e9a2feed9cef7ec5
Ontheflywaste <ontheflywastesolutions@gmail.com>
2025-07-11 11:37:46 -0400

    Center hero text more with better vertical positioning

--- src/pages/JunkRemovalService.tsx
+++ src/pages/JunkRemovalService.tsx
@@ -76,7 +76,7 @@ function JunkRemovalService() {
           <div className="absolute inset-0 bg-gradient-to-r from-black/60 to-transparent" />
           
           <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-full">
-            <div className="flex flex-col justify-center items-center h-full text-center pt-32 pb-16">
+            <div className="flex flex-col justify-center items-center h-full text-center pt-40 pb-24">
               <ScrollReveal>
                 <div className="mb-6">
                   <span className="inline-block px-4 py-2 bg-[#049704]/20 text-[#049704] rounded-full text-sm font-medium backdrop-blur-sm border border-[#049704]/30">

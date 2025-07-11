22121e2dae5d2b17954a310fdf727c8e5c9a1094
Ontheflywaste <ontheflywastesolutions@gmail.com>
2025-07-11 14:35:14 -0400

    Change pricing section background color

--- src/pages/JunkRemovalService.tsx
+++ src/pages/JunkRemovalService.tsx
@@ -282,7 +282,7 @@ function JunkRemovalService() {
         </section>
 
         {/* Pricing Information */}
-        <section className="py-20 bg-white">
+        <section className="py-20 bg-gray-50">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
               <ScrollReveal direction="left">

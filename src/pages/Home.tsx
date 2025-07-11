5c3d5a224ac6cdbdf0c471dd7a9af71819079244
Ontheflywaste <ontheflywastesolutions@gmail.com>
2025-07-11 13:50:13 -0400

    Make membership logos slightly larger

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -170,11 +170,11 @@ function Home() {
                   <img 
                     src="/Images/AAGO.png" 
                     alt="Apartment Association of Greater Orlando Member" 
-                    className="h-16 sm:h-20 md:h-24 object-contain mx-auto mb-3 transition-transform duration-300 group-hover:scale-105"
+                    className="h-20 sm:h-24 md:h-28 object-contain mx-auto mb-3 transition-transform duration-300 group-hover:scale-105"
                     loading="lazy"
                     decoding="async"
-                    width="96"
-                    height="96"
+                    width="112"
+                    height="112"
                   />
                   <p className="text-sm text-gray-700 text-center">Apartment Association of Greater Orlando</p>
                 </a>
@@ -190,11 +190,11 @@ function Home() {
                   <img 
                     src="/Images/faa-full-color-full-logo.png" 
                     alt="Florida Apartment Association Member" 
-                    className="h-16 sm:h-20 md:h-24 object-contain mx-auto mb-3 transition-transform duration-300 group-hover:scale-105"
+                    className="h-20 sm:h-24 md:h-28 object-contain mx-auto mb-3 transition-transform duration-300 group-hover:scale-105"
                     loading="lazy"
                     decoding="async"
-                    width="96"
-                    height="96"
+                    width="112"
+                    height="112"
                   />
                   <p className="text-sm text-gray-700 text-center">Florida Apartment Association</p>
                 </a>
@@ -210,11 +210,11 @@ function Home() {
                   <img 
                     src="/Images/NAA-logo_bgwhite.png" 
                     alt="National Apartment Association Member" 
-                    className="h-16 sm:h-20 md:h-24 object-contain mx-auto mb-3 transition-transform duration-300 group-hover:scale-105"
+                    className="h-20 sm:h-24 md:h-28 object-contain mx-auto mb-3 transition-transform duration-300 group-hover:scale-105"
                     loading="lazy"
                     decoding="async"
-                    width="96"
-                    height="96"
+                    width="112"
+                    height="112"
                   />
                   <p className="text-sm text-gray-700 text-center">National Apartment Association</p>
                 </a>

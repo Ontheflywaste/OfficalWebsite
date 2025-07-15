78d92b5cb7fd03b8c2262b33f23e9c8c2801b701
Ontheflywaste <ontheflywastesolutions@gmail.com>
2025-07-15 14:10:07 -0400

    Create Moving Services Page

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import BlogPost from './pages/BlogPost';
 import ValetTrashService from './pages/ValetTrashService';
 import JunkRemovalService from './pages/JunkRemovalService';
 import PressureWashingService from './pages/PressureWashingService';
+import MovingServices from './pages/MovingServices';
 import useScrollToTop from './hooks/useScrollToTop';
 import ScrollToTop from './components/ScrollToTop';
 import SkipToMain from './components/SkipToMain';
@@ -39,6 +40,7 @@ function AnimatedRoutes() {
         <Route path="/services/valet-trash" element={<PageTransition><ValetTrashService /></PageTransition>} />
         <Route path="/services/junk-removal" element={<PageTransition><JunkRemovalService /></PageTransition>} />
         <Route path="/services/pressure-washing" element={<PageTransition><PressureWashingService /></PageTransition>} />
+        <Route path="/services/moving" element={<PageTransition><MovingServices /></PageTransition>} />
       </Routes>
     </AnimatePresence>
   );

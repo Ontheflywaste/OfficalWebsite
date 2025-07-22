a93f03c7b939d9611eb3120bce49026ff0f1d65b
Ontheflywaste <ontheflywastesolutions@gmail.com>
2025-07-22 14:42:11 -0400

    Fix JunkRemovalService.tsx missing default export

---
+++ src/pages/JunkRemovalService.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const JunkRemovalService: React.FC = () => {
+  return (
+    <div>
+      <h1>Junk Removal Service</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+};
+
+export default JunkRemovalService;

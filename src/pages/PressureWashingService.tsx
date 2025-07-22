f51d8cf4ed9e9c2856d0ffcaa66795f7282a8f65
Ontheflywaste <ontheflywastesolutions@gmail.com>
2025-07-22 14:48:04 -0400

    Fix PressureWashingService.tsx missing default export

---
+++ src/pages/PressureWashingService.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PressureWashingService: React.FC = () => {
+  return (
+    <div>
+      <h1>Pressure Washing Service</h1>
+      <p>Pressure washing service page content coming soon.</p>
+    </div>
+  );
+};
+
+export default PressureWashingService;

d7694b6b37776ba3d0575b13eff4afc2558bdb9a
Ontheflywaste <ontheflywastesolutions@gmail.com>
2025-09-22 19:05:37 -0400

    Update Proud Members Of section background with IMG_9868.JPG

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -560,8 +560,8 @@ function Home() {
 
         {/* Membership Badges Section */}
         <section className="py-12 bg-white relative overflow-hidden">
-          <div className="absolute inset-0 bg-[url('/Images/Astonatuptown.jpg')] opacity-30" style={{ backgroundPosition: 'center 30%', backgroundSize: '90%', backgroundRepeat: 'no-repeat' }} />
-          <div className="absolute inset-0 bg-gradient-to-b from-white/50 to-white/70" />
+          <div className="absolute inset-0 bg-[url('/Images/IMG_9868.JPG')] opacity-30" style={{ backgroundPosition: 'center 30%', backgroundSize: '90%', backgroundRepeat: 'no-repeat' }} />
+          <div className="absolute inset-0 bg-gradient-to-b from-green-50/80 to-white/70" />
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <ScrollReveal>
               <div className="text-center mb-8 relative z-10">

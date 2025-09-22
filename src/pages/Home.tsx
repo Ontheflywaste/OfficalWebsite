55c7fb5a53e89cb397ed0d22cc6744d2548022ca
Ontheflywaste <ontheflywastesolutions@gmail.com>
2025-09-22 19:26:20 -0400

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -560,8 +560,8 @@ function Home() {
 
         {/* Membership Badges Section */}
         <section className="py-12 bg-white relative overflow-hidden">
-          <div className="absolute inset-0 bg-[url('/Images/futuremillionaire.JPG')] opacity-40" style={{ backgroundPosition: 'center 45%', backgroundSize: '90%', backgroundRepeat: 'no-repeat' }} />
-          <div className="absolute inset-0 bg-gradient-to-b from-green-50/60 to-white/50" />
+          <div className="absolute inset-0 bg-[url('/Images/futuremillionaire.JPG')] opacity-30" style={{ backgroundPosition: 'center 30%', backgroundSize: '90%', backgroundRepeat: 'no-repeat' }} />
+          <div className="absolute inset-0 bg-gradient-to-b from-green-50/80 to-white/70" />
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <ScrollReveal>
               <div className="text-center mb-8 relative z-10">

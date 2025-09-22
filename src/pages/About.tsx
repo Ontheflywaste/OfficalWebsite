8836dadd3b23bb93f8a8a7ca7208af91767d93bf
Ontheflywaste <ontheflywastesolutions@gmail.com>
2025-09-22 18:42:20 -0400

    Adjust Trevor Alston's image to match other team members

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -187,12 +187,6 @@ function About() {
                               objectPosition: member.name === "Trevor Alston" ? "50% 50%" : 
                                             member.name === "Donnell Edwards" ? "50% 40%" : "50% 48%"
                             }}
-                            style={{
-                              objectFit: member.name === "Donnell Edwards" ? "contain" : 
-                                        member.name === "Trevor Alston" ? "contain" : "cover",
-                              objectPosition: member.name === "Trevor Alston" ? "50% 50%" : 
-                                            member.name === "Donnell Edwards" ? "50% 50%" : "50% 48%"
-                            }}
                             loading="lazy"
                             decoding="async"
                             onError={(e) => {

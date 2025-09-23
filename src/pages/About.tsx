7cc225a230799170f4346d69787df760cbabf9e3
Ontheflywaste <ontheflywastesolutions@gmail.com>
2025-09-22 22:03:13 -0400

    Fix Trevor Alston image URL

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -60,7 +60,7 @@ function About() {
         
         {/* Preload team member images for faster loading */}
         <link rel="preload" href="https://stackblitz.com/storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBNmJuNEE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--07360e0108b586ff25a79c440b0a804ee03e86c9/-Donnell2.jpg" as="image" />
-        <link rel="preload" href="https://stackblitz.com/storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBNmJuNEE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--54dc6e7dc450fb7613a1a45e82e9b44bb7e6df01/TrevorAlston.jpg" as="image" />
+        <link rel="preload" href="/Images/team/Trevor-Alston.jpg" as="image" />
         <link rel="preload" href="https://stackblitz.com/storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBeVRvNEE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--54dc6e7dc450fb7613a1a45e82e9b44bb7e6df01/-Steven2.jpg" as="image" />
       </Helmet>
 
@@ -156,7 +156,7 @@ function About() {
                 {
                   name: "Trevor Alston",
                   title: "Vice President",
-                  image: "https://stackblitz.com/storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBNmJuNEE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--54dc6e7dc450fb7613a1a45e82e9b44bb7e6df01/TrevorAlston.jpg",
+                  image: "/Images/team/Trevor-Alston.jpg",
                   bio: "Trevor brings a wealth of operational expertise and strategic vision to On The Fly Waste Solutions. His innovative approach to service delivery and team management has been instrumental in our company's growth and success. With a focus on continuous improvement and customer satisfaction, Trevor ensures that our services consistently exceed client expectations while maintaining the highest standards of efficiency and reliability.",
                   linkedin: "https://www.linkedin.com/in/on-the-fly-waste-solutions-23a48b296/",
                   hasImage: true,

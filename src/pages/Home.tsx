7bf00290c6fb40d41e16e950d02162b87a32e18f
Ontheflywaste <ontheflywastesolutions@gmail.com>
2025-09-22 22:36:55 -0400

    Lower background image opacity slightly

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -560,7 +560,7 @@ function Home() {
 
         {/* Membership Badges Section */}
         <section className="py-12 bg-white relative overflow-hidden">
-          <div className="absolute inset-0 bg-[url('/Images/futuremillionaire.JPG')] bg-cover bg-center opacity-30" />
+          <div className="absolute inset-0 bg-[url('/Images/futuremillionaire.JPG')] bg-cover bg-center opacity-20" />
           <div className="absolute inset-0 bg-gradient-to-b from-[#049704]/20 to-[#049704]/30" />
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <ScrollReveal>

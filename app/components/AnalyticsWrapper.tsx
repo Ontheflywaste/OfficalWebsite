03ccbbd2ce92a497ee9b9d1940b83842f94dccfa
Ontheflywaste <ontheflywastesolutions@gmail.com>
2026-04-04 12:10:32 -0400

    Fix build: Disable AnalyticsWrapper

--- app/components/AnalyticsWrapper.tsx
+++ app/components/AnalyticsWrapper.tsx
@@ -1,30 +1,5 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-
 export default function AnalyticsWrapper() {
-  const [AnalyticsComponent, setAnalyticsComponent] = useState<React.ComponentType | null>(null);
-
-  useEffect(() => {
-    let isMounted = true;
-
-    import('@vercel/analytics/react')
-      .then((mod) => {
-        if (isMounted) {
-          setAnalyticsComponent(() => mod.Analytics);
-        }
-      })
-      .catch(() => {
-      });
-
-    return () => {
-      isMounted = false;
-    };
-  }, []);
-
-  if (!AnalyticsComponent) {
-    return null;
-  }
-
-  return <AnalyticsComponent />;
+  return null;
 }
